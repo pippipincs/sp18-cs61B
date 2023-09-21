9a597f5177e9c907d2be2caf961f12ab4e14ca60
catofhim <agnesspei731@gmail.com>
2023-09-21 18:34:49 +0800

    build a Planet class and its constructor, add a method calculating the distance.

---
+++ proj0/Planet.java
@@ -0,0 +1,34 @@
+public class Planet {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+    public Planet(double xP, double yP, double xV, double yV, double m, String img){
+        this.xxPos=xP;
+        this.yyPos=yP;
+        this.xxVel=xV;
+        this.yyVel=yV;
+        this.mass=m;
+        this.imgFileName=img;
+
+    } 
+    public Planet(Planet p){
+        this.xxPos=p.xxPos;
+        this.yyPos=p.yyPos;
+        this.xxVel=p.xxVel;
+        this.yyVel=p.yyVel;
+        this.mass=p.mass;
+        this.imgFileName=p.imgFileName;
+    }
+
+    public double calcDistance(Planet p){
+        
+        double dx=p.xxPos-this.xxPos;
+        double dy=p.yyPos-this.yyPos;
+        double d=Math.sqrt(dx*dx+dy*dy);
+        return d;
+
+    }
+}
