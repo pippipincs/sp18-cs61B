3f2ef56ca62ad130d0baf8bae8ac5bdc7f317604
catofhim <agnesspei731@gmail.com>
2023-09-21 23:33:48 +0800

    add the readPlanet method in NBody class

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -6,7 +6,18 @@ public class NBody {
         In in = new In(file_path);
         int t= in.readInt();
         return in.readDouble();
-
-
     } 
+    public static Planet[] readPlanets(String file_path){
+        In in= new In(file_path);
+        int n=in.readInt();
+        in.readDouble();
+        Planet[] array=new Planet[n];
+        for(int i=0;i<n;i++){
+            array[i]=new Planet(in.readDouble(),in.readDouble(),in.readDouble(),in.readDouble(),in.readDouble(),in.readString());
+        
+        }
+        return array;
+
+
+    }
 }
